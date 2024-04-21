6f43f52fb44f933ad9b7494a706f46fb84b40399
Raphael <raphael-lacerda75@hotmail.com>
2024-04-21 17:08:56 -0300

    adicionando live server

--- Downloads/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Downloads/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30');
+let numeroSecreto = 8;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10');
 
 if(numeroSecreto == chute ) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto, ${numeroSecreto}`);
+}
+else {
+    alert('Você errou, burro!')
 }
