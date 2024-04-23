ac84ad63831b97dc4949a3041b17b3debfd3544d
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-04-23 14:22:57 -0300

    adicionando número aleatório

--- Downloads/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ Downloads/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,11 +1,32 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 8;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+//A função parseInt() analisa um argumento string e retorna um inteiro na base especificada.
+//A função Math.random() é usado para retornar um número pseudoaleatório de ponto flutuante entre 0 e 1(exclusivo)
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let chute;
+let tentativas = 1;
 
-if(numeroSecreto == chute ) {
-    alert(`Isso ai! Você descobriu o número secreto, ${numeroSecreto}`);
+//enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 100'); 
+    if(numeroSecreto == chute ) {
+        break;
+    }
+    else {
+        if(numeroSecreto > chute){
+            alert(`O número secreto é maior que ${chute}`);
+        } else{
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;    
+    }
 }
-else {
-    alert('Você errou, burro!')
-}
+// Operador ternário, mesma coisa que IF e Else
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto, ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+//if(tentativas > 1) {
+//    alert(`Isso ai! Você descobriu o número secreto, ${numeroSecreto} com ${tentativas} tentativas`);
+//}else {
+//    alert(`Isso ai! Você descobriu o número secreto, ${numeroSecreto} com ${tentativas} tentativa`);
+//}
